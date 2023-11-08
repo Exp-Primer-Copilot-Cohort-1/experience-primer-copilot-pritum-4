c5cc8119b0e9af152682dd54d0526220a005aeeb
pritum-4 <104716871+pritum-4@users.noreply.github.com>
2023-11-08 17:47:16 +0000

    Copilot first commit

--- skills.js
+++ skills.js
@@ -1,5 +1,8 @@
-function calculateNumbers(var1, var2)  
-{
-    console.log(var1+var2);
-    
+function calculateNumbers() {
+    var field1 = parseInt(document.getElementById("field1").value);
+    var field2 = parseInt(document.getElementById("field2").value);
+    var result = field1 * field2;
+    if (!isNaN(result)) {
+        document.getElementById("result").innerHTML = "The result is " + result;
+    }
 }
